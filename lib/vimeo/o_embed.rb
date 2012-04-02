af43b60db7230e9e30e343fe1d22cb8ee5ef2916
Moritz Kröger <moritz.kroeger@googlemail.com>
2012-04-02 15:47:40 +0200

    made embed get_info method class method.

--- lib/vimeo/o_embed.rb
+++ lib/vimeo/o_embed.rb
@@ -2,7 +2,7 @@ module Vimeo
   class OEmbed
     include HTTParty
 
-    def get_info(video_id, params=nil)
+    def self.get_info(video_id, params=nil)
       query_url = "http://vimeo.com/api/oembed.json?url=http%3A//vimeo.com/#{video_id}"
       query_url += "&"+params.to_query if params
       HTTParty::get(query_url)
